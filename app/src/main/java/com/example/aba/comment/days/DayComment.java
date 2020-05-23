552af61e55b7aa4d7943a96fcbb8acfe604c2928
Maksym Budzin <maxi985049@gmail.com>
2020-05-23 16:56:37 +0300

    Fixed bugs that allows us to accumulate comments in the database in the table responsible for performing tasks in the updated project

--- app/src/main/java/com/example/aba/comment/days/DayComment.java
+++ app/src/main/java/com/example/aba/comment/days/DayComment.java
@@ -1,5 +1,6 @@
 package com.example.aba.comment.days;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,8 +14,11 @@ import com.example.aba.comment.Comment;
 import com.example.aba.R;
 import com.example.aba.menuActivity.Menu;
 import com.example.aba.users.UserDetails;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -27,6 +31,7 @@ public class DayComment extends AppCompatActivity {
     EditText editComment;
     DatabaseReference reff;
     Comment comment;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +41,27 @@ public class DayComment extends AppCompatActivity {
         Calendar calendar =Calendar.getInstance();
         String currentData = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
         reff = FirebaseDatabase.getInstance().getReference().child("users/"+ UserDetails.username+"/kids/").child(UserDetails.kidName).child("comments/"+currentData+"/comment");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    i = (int) dataSnapshot.getChildrenCount();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         comment = new Comment();
         btnAdd.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 comment.setComment(editComment.getText().toString().trim());
-                reff.setValue(comment);
+                reff.child(String.valueOf(i)).setValue(comment);
                 Toast.makeText(DayComment.this,"comment was sent",Toast.LENGTH_LONG).show();
                 openMenu();
             }
