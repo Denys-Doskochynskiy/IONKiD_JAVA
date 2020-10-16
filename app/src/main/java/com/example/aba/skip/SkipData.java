8fe3ddeb674cd1a016e17c8336c903ebe4b4ac32
Denys-Doskochynskiy <Denys-Doskochynskiy@iONKiD.github.com>
2020-10-16 16:50:29 +0300

    Make some minor fix with skip

--- app/src/main/java/com/example/aba/skip/SkipData.java
+++ app/src/main/java/com/example/aba/skip/SkipData.java
@@ -1,3 +1,18 @@
+/*
+ *
+ *   Created Your Name on 16.10.20 16:50
+ *   Copyright Ⓒ 2020. All rights reserved Ⓒ 2020 http://freefuninfo.com/
+ *   Last modified: 16.10.20 16:46
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENS... Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * /
+ */
+
 package com.example.aba.skip;
 
 import androidx.annotation.NonNull;
@@ -31,7 +46,7 @@ public class SkipData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skip_data);
         final FirebaseFirestore dbFirestore = FirebaseFirestore.getInstance();
-        reff = FirebaseDatabase.getInstance().getReference().child("users/" + UserDetails.username + "/kids/").child(UserDetails.kidName).child("SKIP");
+        reff = FirebaseDatabase.getInstance().getReference().child("users/" + UserDetails.username + "/KidInfo/").child(UserDetails.kidName).child("SKIP");
         reff.addValueEventListener(new ValueEventListener() {
 
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -42,7 +57,7 @@ public class SkipData extends AppCompatActivity {
                 skipData.put("Status",status);
                 skipData.put("Log",log);
 
-                dbFirestore.collection("UserInfo").document(UserDetails.username).collection("kids").document(UserDetails.kidName).set(skipData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                dbFirestore.collection("UserInfo").document(UserDetails.username).collection("KidInfo").document(UserDetails.kidName).set(skipData).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         Toast.makeText(SkipData.this,status,
